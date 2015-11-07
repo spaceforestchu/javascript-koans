ddbd94e0bb26bae23386d3c4d0852d62d31a582e
William <William@Williams-MBP-2.fios-router.home>
2015-11-06 19:08:07 -0500

    third fix

--- koans/AboutExpects.js
+++ koans/AboutExpects.js
@@ -10,7 +10,7 @@ describe("About Expects", function() {
     var expectedValue = FILL_ME_IN;
     var actualValue = 1 + 1;
 
-    expect(actualValue === expectedValue).toBeTruthy();
+    expect(actualValue !== expectedValue).toBeTruthy();
   });
 
   // Some ways of asserting equality are better than others.
@@ -19,7 +19,7 @@ describe("About Expects", function() {
     var actualValue = 1 + 1;
 
   // toEqual() compares using common sense equality.
-    expect(actualValue).toEqual(expectedValue);
+    expect(actualValue) === (expectedValue);
   });
 
   // Sometimes you need to be really exact about what you "type."
